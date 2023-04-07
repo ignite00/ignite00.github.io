329edbf361a4922e175eed31946c6169c77e7e9d
Dylan Tan <108455199+TechWizGYT@users.noreply.github.com>
2023-04-07 09:40:44 +0930

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -41,5 +41,5 @@ const quotes = [
     "Oh sh... - Lilian 2023",
     "So hopefully he will fix the white issue - Dylan 2023",
     "ThAtS whAT shE sAId! - Ming Lang 2023",
-    "Well f*&k them! - Dylan Tan 2023"
+    "ITS LILIAN HERE AND I SAY BALLS ARE NOT TASTY - Lilian 2023",
     ];
