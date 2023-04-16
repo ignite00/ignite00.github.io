f7043faedc9231e611e4cdd563490cd13a3550e4
Dylan Tan <108455199+TechWizGYT@users.noreply.github.com>
2023-04-16 19:07:36 +0930

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -98,5 +98,7 @@ const quotes = [
     "Bo’lle o’ wa’er - Kerui 2023",
     "Can I borrow the sharpener... again? - Tyler 2023",
     "Why are there big flying sheep in the sky? - Ming Lang 2023",
-    "✨OOH SHINY BUTTON✨ - Lilian 2023",
+    "I don't press things if there is no ✨OOH SHINY BUTTON✨ - Lilian 2023",
+    "THAT'S CALLED EXLUSION - Lilian 2023",
+
     ]; 
