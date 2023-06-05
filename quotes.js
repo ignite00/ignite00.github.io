d901336698b81f320c6aa08633235158c9a0a905
Dylan Tan <108455199+TechWizGYT@users.noreply.github.com>
2023-06-05 19:24:00 +0930

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -164,4 +164,5 @@ const quotes = [
     "When Dylan says oops, it's likely a catastrophic disaster",
     "I AM THE CALCULATOR - Lukas 2023",
     "Dylan is the only leader - Mehrad 2023",
+    "Let's hope Jesper doesn't have COVID, let's hope he has cancer! - Kerui 2023",
     ]; 
